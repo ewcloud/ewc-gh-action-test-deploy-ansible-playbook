98481a642abbddbb619f8591f085de6ac0cc2492
Mario Loaiciga <jorge.mlr.elec@gmail.com>
2026-01-29 17:46:35 +0100

    fix: Mute SIGPIPE / broken pipe condition by jq output clipping (#7)

--- test-deploy-ansible-playbook.sh
+++ test-deploy-ansible-playbook.sh
@@ -206,7 +206,7 @@ if [ -f "$ARTIFACTS_DIR/sbom.json" ]; then
   add_summary ">💡 This section contains only an excerpt of the SBOM. Checkout the sbom.json file, attached as part of the run artifacts, for complete information on installed dependencies."
   add_summary ""
   add_summary "\`\`\`json"
-  jq . "$ARTIFACTS_DIR/sbom.json"  | head -n 150 >> "$GITHUB_STEP_SUMMARY"
+  jq . "$ARTIFACTS_DIR/sbom.json"  | head -n 150 >> "$GITHUB_STEP_SUMMARY" || true
   add_summary ""
   add_summary "    ..."
   add_summary ""
