8b121d629ba540d57e52e7f9db83ba6f33c4226a
slothkong <jorge.mlr.elec@gmail.com>
2026-08-25 18:09:58 +0000

    style: Reflect GitHub run as part of provisioned VMs

--- test-deploy-ansible-playbook.sh
+++ test-deploy-ansible-playbook.sh
@@ -55,7 +55,7 @@ cat > /tmp/vars.tfvars <<EOF
 image_name            = "${OS_IMAGE_NAME}"
 flavor_name           = "${OS_FLAVOR_NAME}"
 app_name              = "${INSTANCE_NAME_PREFIX}"
-instance_name         = "vm"
+instance_name         = "run"
 instance_index        = ${GITHUB_RUN_ID}
 networks              = ["${OS_PRIVATE_NETWORK_NAME}"]
 external_network_name = "${OS_EXTERNAL_NETWORK_NAME}"
@@ -228,6 +228,6 @@ terraform destroy -auto-approve -var-file=/tmp/vars.tfvars
 # -- Step 19 ---
 echo "Re-rasing test errors (if any)"
 if [ "$TERRAFORM_STATUS" = "failing" ] || [ "$ANSIBLE_STATUS" = "failing" ]; then
-  echo "::error::One or more failures caught during testing. See the summary or logs for details"
+  echo "::error::Deployment test(s) FAILING!. See the Summary or logs for details"
   exit 1
 fi
